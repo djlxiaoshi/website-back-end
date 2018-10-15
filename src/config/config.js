0528833011139dba4c1238bf95b0458646003557
1281233206@qq.com <1281233206@qq.com>
2018-10-15 21:16:10 +0800

    处理预请求

--- src/config/config.js
+++ src/config/config.js
@@ -16,5 +16,6 @@ module.exports = {
     signed: true, /** (boolean) signed or not (default true) */
     rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
     renew: false, /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
-  }
+  },
+  allowOrigin: 'http://localhost:8080'
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,26 @@
 
 const Router = require('koa-router');
 const userRouter = require('./user');
+const config = require('../config/config');
 
 const router = new Router();
 
 // api cors
 router.use(async (ctx, next) => {
   ctx.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Origin', config.allowOrigin);
+  ctx.set('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
   ctx.set('Access-Control-Allow-Credentials', true);
   await next()
+});
+
+// 处理预请求
+// api options method
+router.options('*', async (ctx, next) => {
+  ctx.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  ctx.set('Access-Control-Allow-Origin', config.allowOrigin);
+  ctx.status = 204;
+  await next();
 });
 
 // 用户相关路由
